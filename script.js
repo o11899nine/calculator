e71d78727e094d6c02006422b7168ae1b1ba73f1
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2023-07-13 15:35:47 +0200

    Make setOperator function call calculate() function when there is something to calculate

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const equalsButton = document.getElementById("equals-btn");
 const operatorButtons = document.querySelectorAll(".operator-btn");
 
 digitButtons.forEach(button => {
-  button.addEventListener("click", updateDisplayValue)
+  button.addEventListener("click", addDigit)
 });
 equalsButton.addEventListener("click", calculate);
 
 operatorButtons.forEach(button => {
-  button.addEventListener("click", selectOperator)
+  button.addEventListener("click", setOperator)
 });
 
 // Math functions
@@ -23,16 +23,14 @@ let displayValue = "";
 let operator;
 let num1;
 let num2;
+let solution;
 
-
-// cijfers duwen = aan string toevoegen (variabele input)
-// operator duwen = variabele input opslaan in andere variabele, string leegmaken, operator pslaan
-// cijfers duwen = aan string toevogen (variable input)
-// = duwen = som
 
 function calculate() {
-  num2 = parseInt(displayValue);
-  displayValue = operate(operator, num1, num2);
+  num2 = parseFloat(displayValue);
+  solution = operate(operator, num1, num2);
+  num1 = solution;
+  displayValue = solution;
   updateDisplay(displayValue);
 }
 
@@ -47,18 +45,25 @@ function operate(operator, a, b) {
     case "divide":
       return divide(a, b);
   }
-
 }
 
 
-function selectOperator() {
-  operator = this.dataset.operator;
-  num1 = parseInt(displayValue);
+function setOperator(event) {
+  if (num1 !== undefined) {
+    console.log("there is a num1");
+    calculate();
+  }
+  operator = event.target.dataset.operator;
+  num1 = parseFloat(displayValue);
   displayValue = "";
+
+  if (solution !== null) {
+    
+  }
 }
 
-function updateDisplayValue() {
-  displayValue += this.dataset.digit;
+function addDigit(event) {
+  displayValue += event.target.dataset.digit;
   updateDisplay(displayValue);
 }
 
