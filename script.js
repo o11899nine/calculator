52592cc31c9f1889cd01868f3defdb2ffbd82062
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2023-07-14 10:29:11 +0200

    Improve calculate() function for use in multiple places

--- script.js
+++ script.js
@@ -25,16 +25,15 @@ let numbers = [];
 let solution = 0;
 
 function calculate() {
-  // console.log("calculate()");
-  num2 = parseFloat(newNumberString);
-  // console.log(`num2: ${num2}`);
-  solution = operate(operator, num1, num2);
-  // console.log(`solution: ${solution}`);
-  num1 = solution;
-  // console.log(`num1: ${num1}`);
-  newNumberString = solution;
-  // console.log(`displayValue: ${displayValue}`);
-  updateDisplay(newNumberString);
+  if (newNumberString !== "") {
+    numbers.push(parseFloat(newNumberString));
+  }
+  if (numbers.length > 1) {
+    solution = operate(numbers[0], numbers[1]);
+    numbers = [solution];
+    updateDisplay(solution)
+  } 
+  newNumberString = "";
 }
 
 
@@ -54,25 +53,13 @@ function operate(a, b) {
 
 
 function setOperator(event) {
-  // console.log("setOperator()");
+  calculate();
   operator = event.target.dataset.operator;
-  // console.log(`operator: ${operator}`);
-  if (newNumberString !== "") {
-    numbers.push(parseFloat(newNumberString));
-  }
-
-  if (numbers.length > 1) {
-    solution = operate(numbers[0], numbers[1]);
-    numbers = [solution];
-    updateDisplay(solution)
-  } 
-  newNumberString = "";
+  
 }
 
 function addDigit(event) {
-  // console.log("addDigit()");
   newNumberString += event.target.dataset.digit;
-  // console.log(`displayValue: ${displayValue}`);
   updateDisplay(newNumberString);
 }
 
