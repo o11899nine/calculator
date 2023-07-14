635d4a391b5000fe20d1d264f4e3a0226c778728
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2023-07-14 10:17:44 +0200

    Use array to store numbers. Update display with solution when operator is clicked and there is something to be calculated

--- script.js
+++ script.js
@@ -19,29 +19,27 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 // Global variables
-let displayValue = "";
+let newNumberString = "";
 let operator;
-let num1;
-let num2;
-let solution;
-
+let numbers = [];
+let solution = 0;
 
 function calculate() {
-  console.log("calculate()");
-  num2 = parseFloat(displayValue);
-  console.log(`num2: ${num2}`);
+  // console.log("calculate()");
+  num2 = parseFloat(newNumberString);
+  // console.log(`num2: ${num2}`);
   solution = operate(operator, num1, num2);
-  console.log(`solution: ${solution}`);
+  // console.log(`solution: ${solution}`);
   num1 = solution;
-  console.log(`num1: ${num1}`);
-  displayValue = solution;
-  console.log(`displayValue: ${displayValue}`);
-  updateDisplay(displayValue);
+  // console.log(`num1: ${num1}`);
+  newNumberString = solution;
+  // console.log(`displayValue: ${displayValue}`);
+  updateDisplay(newNumberString);
 }
 
 
-function operate(operator, a, b) {
-  console.log("operate()");
+function operate(a, b) {
+  // console.log("operate()");
   switch (operator) {
     case "add":
       return add(a, b);
@@ -56,20 +54,26 @@ function operate(operator, a, b) {
 
 
 function setOperator(event) {
-  console.log("setOperator()");
+  // console.log("setOperator()");
   operator = event.target.dataset.operator;
-  console.log(`operator: ${operator}`);
-  num1 = parseFloat(displayValue);
-  console.log(`num1: ${num1}`);
-  displayValue = "";
-  console.log(`displayValue: ${displayValue}`);
+  // console.log(`operator: ${operator}`);
+  if (newNumberString !== "") {
+    numbers.push(parseFloat(newNumberString));
+  }
+
+  if (numbers.length > 1) {
+    solution = operate(numbers[0], numbers[1]);
+    numbers = [solution];
+    updateDisplay(solution)
+  } 
+  newNumberString = "";
 }
 
 function addDigit(event) {
-  console.log("addDigit()");
-  displayValue += event.target.dataset.digit;
-  console.log(`displayValue: ${displayValue}`);
-  updateDisplay(displayValue);
+  // console.log("addDigit()");
+  newNumberString += event.target.dataset.digit;
+  // console.log(`displayValue: ${displayValue}`);
+  updateDisplay(newNumberString);
 }
 
 function updateDisplay(value) {
